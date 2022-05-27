d6737f2a380579fb29cb6b1d21b305ff80506790
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2022-05-27 21:02:36 +0700

    Add files via upload

---
+++ Looping.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // while loop
+  var start = 1;
+  while (start <= 10) {
+    print(start);
+    start++;
+  }
+
+  print('==============================');
+
+  // for loop
+  for (var i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
